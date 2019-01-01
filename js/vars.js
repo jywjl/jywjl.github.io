c8caed1665c34df3536a0c7dfda9d58b8678486c
StevenZack <stevenzack@qq.com>
2019-01-01 15:43:47 +0800

    Just a Backup

--- js/vars.js
+++ js/vars.js
@@ -1,4 +1,4 @@
 url_windows='windows'
-url_linux='linux'
+url_linux='http://files.git.oschina.net/group1/M00/06/1E/PaAvDFwrGeKAdvooAEsgmsPc8i0707.bz2?token=427d1a92fe1d50a65a77ed929587cb94&ts=1546328560&attname=jywjl.tar.bz2'
 url_mac='mac'
-url_android='android'
+url_android='http://files.git.oschina.net/group1/M00/06/1E/PaAvDFwrGZuAQiHkAMJpd9ujsHw308.apk?token=1863e0d556da1dc9d542b14c08897d11&ts=1546328560&attname=jywjl_v1.0.0.apk'
