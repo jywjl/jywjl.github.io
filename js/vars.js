23ce1a0363f20f307fdbbb928882c1f09e2cd0ce
stevenzack <zhouyuhao@xiaoheiban.cn>
2019-08-20 20:25:39 +0800

    Just a Backup

--- js/vars.js
+++ js/vars.js
@@ -4,13 +4,13 @@ version_history={
             version:'v2.0.0',
             time:'--',
             description:'',
-            url:'https://files.gitee.com/group1/M00/08/01/PaAvDFz9yEWAYN1NAQ-eZJXh7pw684.exe?token=2548e4712e5fb8ff3dd8a94a9f223648&ts=1565757875&attname=jywjl_v2.0.0.exe&disposition=attachment'
+            url:'https://coding.net/s/3158fccd-f6e4-41de-9d97-136cce0e0cee'
         },
         {
             version:'v1.0.0',
             time:'--',
             description:'',
-            url:'https://files.gitee.com/group1/M00/08/01/PaAvDFz9yb2AP8nHAQ8E17THoo8338.exe?token=fd511c1f962ec0510a44a571f69efc2e&ts=1565757896&attname=jywjl_v1.0.0.exe&disposition=attachment'
+            url:'https://coding.net/s/5172a6bc-4612-4ef0-ae22-e8f3a25f6bc2'
         }
     ],
     android:[
@@ -18,13 +18,13 @@ version_history={
             version:'v2.0.0',
             time:'--',
             description:'',
-            url:'https://files.gitee.com/group1/M00/08/01/PaAvDFz9yXaAUBsmAMTJ-7EbLLI398.apk?token=18ac1304c887023eb038d1351bf32e93&ts=1565757699&attname=jywjl_v2.0.0.apk&disposition=attachment'
+            url:'https://coding.net/s/9a2fe80c-07c1-4a9f-a078-f27a3a23f22d'
         },
         {
             version:'v1.0.0',
             time:'--',
             description:'',
-            url:'https://files.gitee.com/group1/M00/08/01/PaAvDFz9yXaAT0XEAMJr5yC40EM219.apk?token=a4ff11b6cffc2e81bdf01d4ff5fbde18&ts=1565757756&attname=jywjl_v1.0.0.apk&disposition=attachment'
+            url:'https://coding.net/s/66dd8b82-ab68-4828-b123-8b98979aa31e'
         }
     ],
     mac:[
@@ -32,13 +32,13 @@ version_history={
             version:'v2.0.0',
             time:'--',
             description:'',
-            url:'https://files.gitee.com/group1/M00/08/01/PaAvDFz9x-WAc76FAKpUCqRacbE449.zip?token=5df610cd9ab8f9f8beb9558e7a0b2b17&ts=1565757836&attname=jywjl_v2.0.0.zip&disposition=attachment'
+            url:'https://coding.net/s/b8d2393a-07ba-40a3-90e9-eda471ad1167'
         },
         {
             version:'v1.0.0',
             time:'--',
             description:'',
-            url:'https://files.gitee.com/group1/M00/08/01/PaAvDFz9xteAD_a0AKi7dufDPxE055.zip?token=ae887270591e5cec433b566da8e35bd3&ts=1565757857&attname=jywjl_v1.0.0.zip&disposition=attachment'
+            url:'https://coding.net/s/c65251ff-0e8a-43ed-b9c2-ad7c606a1868'
         }
     ],
     linux:[
@@ -46,13 +46,13 @@ version_history={
             version:'v2.0.0',
             time:'--',
             description:'',
-            url:'https://files.gitee.com/group1/M00/08/01/PaAvDFz9yOaAe_alAEwDcXmaJ2E907.bz2?token=f081d037ef7bc695e7af016a0b0fda9f&ts=1565757785&attname=jywjl_v2.0.0.tar.bz2&disposition=attachment'
+            url:'https://coding.net/s/487ec56b-a337-4e01-a392-7caa31175fc2'
         },
         {
             version:'v1.0.0',
             time:'--',
             description:'',
-            url:'https://files.gitee.com/group1/M00/08/01/PaAvDFz9yOaADk1IAEsgOxRqdaQ858.bz2?token=5f850167c0e14015d45e285c166b146d&ts=1565757818&attname=jywjl_v1.0.0.tar.bz2&disposition=attachmenthttps://files.gitee.com/group1/M00/08/01/PaAvDFz9yOaADk1IAEsgOxRqdaQ858.bz2?token=e08dc3c85f614c6ef4f9e050083291e7&ts=1560137230&attname=jywjl_v1.0.0.tar.bz2&disposition=attachment'
+            url:'https://coding.net/s/37fd1a6d-fb45-472e-ac8e-783a6b450504'
         }
     ]
 }
