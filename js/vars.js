15be188d1fb3ae1bbf33a9d13764f7736b287dbc
stevenzack <zigzigcheers@gmail.com>
2020-01-30 11:25:40 +0800

    Just a Backup

--- js/vars.js
+++ js/vars.js
@@ -1,3 +1,9 @@
+urls={
+    android: 'https://github.com/jywjl/jywjl.github.io/releases/download/android/jywjl_android_v3.0.0.apk',
+    windows: 'https://github.com/jywjl/jywjl.github.io/releases/download/windows/jywjl_windows_v2.0.0.exe',
+    linux: 'https://github.com/jywjl/jywjl.github.io/releases/download/linux/jywjl_linux_amd64_v2.0.0.tar.bz2',
+    mac: 'https://github.com/jywjl/jywjl.github.io/releases/download/mac/jywjl_mac_v2.0.0.zip',
+}
 
 version_history = {
     windows: [
