dcf4ee6caf2d72aca854d089b036cedf9fefb18d
stevenzack <zigzigcheers@gmail.com>
2020-02-04 17:13:10 +0800

    gitee

--- js/vars.js
+++ js/vars.js
@@ -1,9 +1,9 @@
 window.server = '120.77.202.220'
 urls = {
-    android: 'https://github.com/jywjl/jywjl.github.io/releases/download/android/jywjl_android_v3.0.0.apk',
-    windows: 'https://github.com/jywjl/jywjl.github.io/releases/download/windows/jywjl_windows_v2.0.0.exe',
-    linux: 'https://github.com/jywjl/jywjl.github.io/releases/download/linux/jywjl_linux_amd64_v2.0.0.tar.bz2',
-    mac: 'https://github.com/jywjl/jywjl.github.io/releases/download/mac/jywjl_mac_v2.0.0.zip',
+    android: 'http://jywjl.gitee.io/binaries/jywjl_android_v3.0.0.apk',
+    windows: 'http://jywjl.gitee.io/binaries/jywjl_windows_v2.0.0.exe',
+    linux: 'http://jywjl.gitee.io/binaries/jywjl_linux_amd64_v2.0.0.tar.bz2',
+    mac: 'http://jywjl.gitee.io/binaries/jywjl_mac_v2.0.0.zip',
 }
 
 version_history = {
