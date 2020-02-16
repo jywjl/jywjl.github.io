192e29585290efb0e8bc72e094483a689eb56758
stevenzack <zhouyuhao@xiaoheiban.cn>
2020-02-16 17:44:19 +0800

    3.1.1

--- js/vars.js
+++ js/vars.js
@@ -1,6 +1,6 @@
 window.server = '120.77.202.220'
 urls = {
-    android: 'http://jywjl.gitee.io/binaries/jywjl_android_v3.1.0.apk',
+    android: 'http://jywjl.gitee.io/binaries/jywjl_android_v3.1.1.apk',
     windows: 'http://jywjl.gitee.io/binaries/jywjl_windows_v2.0.0.exe',
     linux: 'http://jywjl.gitee.io/binaries/jywjl_linux_amd64_v2.0.0.tar.bz2',
     mac: 'http://jywjl.gitee.io/binaries/jywjl_mac_v2.0.0.zip',
