5128e2861f516bf282b215d532ece776187c3b8a
stevenzack <zigzigcheers@gmail.com>
2020-03-24 14:40:13 +0800

    windows_v3.0.1

--- res/js/vars.js
+++ res/js/vars.js
@@ -1,7 +1,7 @@
 window.server = '120.77.202.220'
 urls = {
     android: 'http://jywjl.gitee.io/binaries/jywjl_android_v3.1.4.apk',
-    windows: 'http://jywjl.gitee.io/binaries/jywjl_windows_v3.0.0.exe',
+    windows: 'http://jywjl.gitee.io/binaries/jywjl_windows_v3.0.1.exe',
     linux: 'http://jywjl.gitee.io/binaries/jywjl_linux_amd64_v2.0.0.tar.bz2',
     mac: 'http://jywjl.gitee.io/binaries/jywjl_mac_v2.0.0.zip',
 }
