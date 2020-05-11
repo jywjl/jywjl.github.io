b53cee5bccdd58cfbda9d93f29cb59d1b995cbea
stevenzack <zhouyuhao@xiaoheiban.cn>
2020-05-11 16:22:42 +0800

    github-release

--- res/js/vars.js
+++ res/js/vars.js
@@ -1,7 +1,7 @@
 window.server = '120.77.202.220'
 urls = {
-    android: 'http://jywjl.gitee.io/binaries/jywjl_android_v3.2.3.apk',
-    windows: 'http://jywjl.gitee.io/binaries/jywjl_windows_v3.2.3.exe',
-    linux: 'http://jywjl.gitee.io/binaries/jywjl_linux_amd64_v2.0.0.tar.bz2',
-    mac: 'http://jywjl.gitee.io/binaries/jywjl_mac_v2.0.0.zip',
+    android: 'https://github.com/jywjl/jywjl.github.io/releases/download/android/jywjl_android_v3.2.3.apk',
+    windows: 'https://github.com/jywjl/jywjl.github.io/releases/download/windows/jywjl_windows_v3.2.3.exe',
+    linux: 'https://github.com/jywjl/jywjl.github.io/releases/download/linux/jywjl_linux_amd64_v2.0.0.tar.bz2',
+    mac: 'https://github.com/jywjl/jywjl.github.io/releases/download/mac/jywjl_mac_v2.0.0.zip',
 }
