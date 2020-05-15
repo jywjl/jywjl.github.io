355956a5b2dad0699bf43ef5de0732bfafb392f7
stevenzack <zigzigcheers@gmail.com>
2020-05-16 00:48:58 +0800

    lanzou

--- res/js/vars.js
+++ res/js/vars.js
@@ -1,7 +1,7 @@
 window.server = '120.77.202.220'
 urls = {
-    android: 'https://github.com/jywjl/jywjl.github.io/releases/download/android/jywjl_android_v3.2.3.apk',
-    windows: 'https://github.com/jywjl/jywjl.github.io/releases/download/windows/jywjl_windows_v3.2.3.exe',
-    linux: 'https://github.com/jywjl/jywjl.github.io/releases/download/linux/jywjl_linux_amd64_v2.0.0.tar.bz2',
-    mac: 'https://github.com/jywjl/jywjl.github.io/releases/download/mac/jywjl_mac_v2.0.0.zip',
+    android: 'https://ww.lanzous.com/ico7l1a',
+    windows: 'https://ww.lanzous.com/ico7lhg',
+    linux: 'https://ww.lanzous.com/ico7rje',
+    mac: 'https://ww.lanzous.com/ico7l4d',
 }
