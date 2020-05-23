f8b5544f0878842e7cde20cb9b4d088e83247d63
stevenzack <zhouyuhao@xiaoheiban.cn>
2020-05-23 13:40:05 +0800

    5

--- res/js/vars.js
+++ res/js/vars.js
@@ -4,4 +4,5 @@ urls = {
     windows: 'https://ww.lanzous.com/ico7lhg',
     linux: 'https://ww.lanzous.com/ico7rje',
     mac: 'https://ww.lanzous.com/ico7l4d',
+    ios:'https://apps.apple.com/us/app/%E5%B1%80%E5%9F%9F%E7%BD%91%E7%B2%BE%E7%81%B5pro/id1511699208?l=zh&ls=1'
 }
