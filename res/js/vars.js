78a5f879abb81f64904c8d0d7cf76d41c9541b4d
stevenzack <zigzigcheers@gmail.com>
2020-06-20 10:10:40 +0800

    GOOS

--- res/js/vars.js
+++ res/js/vars.js
@@ -3,6 +3,6 @@ urls = {
     android: 'https://ww.lanzous.com/ico7l1a',
     windows: 'https://ww.lanzous.com/ico7lhg',
     linux: 'https://ww.lanzous.com/ico7rje',
-    mac: 'https://ww.lanzous.com/ico7l4d',
+    darwin: 'https://ww.lanzous.com/ico7l4d',
     ios:'https://apps.apple.com/us/app/%E5%B1%80%E5%9F%9F%E7%BD%91%E7%B2%BE%E7%81%B5pro/id1511699208?l=zh&ls=1'
 }
