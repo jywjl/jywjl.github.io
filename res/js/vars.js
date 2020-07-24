83da1613334972eb0a3b72150e8a071e15cf285b
stevenzack <zigzigcheers@gmail.com>
2020-07-24 14:40:17 +0800

    linux

--- res/js/vars.js
+++ res/js/vars.js
@@ -2,7 +2,7 @@ window.server = '120.77.202.220'
 urls = {
     android: 'https://wwe.lanzous.com/ijqoye1vtdi',
     windows: 'https://ww.lanzous.com/ico7lhg',
-    linux: 'https://ww.lanzous.com/ico7rje',
+    linux: 'https://wwe.lanzous.com/iA99Mexjx4d',
     darwin: 'https://ww.lanzous.com/ico7l4d',
     ios:'https://apps.apple.com/us/app/%E5%B1%80%E5%9F%9F%E7%BD%91%E7%B2%BE%E7%81%B5pro/id1511699208?l=zh&ls=1'
 }
