a941b2ea94785c7995ae8a0c4d86c20194770db3
stevenzack <zigzigcheers@gmail.com>
2021-05-12 15:45:50 +0800

    lanzoux

--- res/js/vars.js
+++ res/js/vars.js
@@ -1,8 +1,8 @@
 window.server = '120.77.202.220'
 urls = {
-    android: 'https://wwi.lanzous.com/iSEPNlbk0ah',
-    windows: 'https://ww.lanzous.com/ico7lhg',
-    linux: 'https://wwe.lanzous.com/iwEVpey3qcj',
-    darwin: 'https://ww.lanzous.com/ico7l4d',
+    android: 'https://wwi.lanzoux.com/iSEPNlbk0ah',
+    windows: 'https://ww.lanzoux.com/ico7lhg',
+    linux: 'https://wwe.lanzoux.com/iwEVpey3qcj',
+    darwin: 'https://ww.lanzoux.com/ico7l4d',
     ios:'https://apps.apple.com/us/app/%E5%B1%80%E5%9F%9F%E7%BD%91%E7%B2%BE%E7%81%B5pro/id1511699208?l=zh&ls=1'
 }
